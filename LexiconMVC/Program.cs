c32ddb9db8c1b26f5fe6f493d6d0e50ce626d413
sky-wolf <sky-wolf@users.noreply.github.com>
2022-10-31 17:03:11 +0100

    Add project files.

---
+++ LexiconMVC/Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMvc();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+app.MapDefaultControllerRoute();
+
+app.Run();
