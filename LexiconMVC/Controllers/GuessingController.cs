973fc050b77b7571e29dc3f5a07666cb86082f6a
sky-wolf <sky-wolf@users.noreply.github.com>
2022-11-03 11:53:06 +0100

    Guessing game

---
+++ LexiconMVC/Controllers/GuessingController.cs
@@ -0,0 +1,68 @@
+﻿using LexiconMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace LexiconMVC.Controllers
+{
+    public class GuessingController : Controller
+    {
+        private readonly IGuessingRepository _guessingRepository;
+        
+        public GuessingController(IGuessingRepository guessingRepository)
+        {
+            _guessingRepository = guessingRepository;
+        }
+
+        public IActionResult Guess()
+        {
+            var generates = _guessingRepository.Generat();
+            HttpContext.Session.SetInt32("SessionKeyGuess", generates);
+            ViewBag.Generated = "A new Number has been generated so start the guessing game";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Guess(Guessing guessing , int count)
+        {
+            CookieOptions options = new CookieOptions();
+            options.Expires = DateTime.Now.AddMinutes(15);
+
+            count++;
+
+            if (HttpContext.Session.GetInt32("SessionKeyGuess") != 0)
+            {
+                var tal = HttpContext.Session.GetInt32("SessionKeyGuess");
+                var vinn = _guessingRepository.Guess(guessing, (int)tal);
+
+                if(vinn)
+                {
+                    ViewBag.Vinn = "Winer winer chicken diner";
+                    HttpContext.Session.Clear();
+                    var highscore = HttpContext.Request.Cookies["higscore"];
+                    if (highscore == null)
+                    {
+                        HttpContext.Response.Cookies.Append("higscore", Convert.ToString(count), options);
+                        ViewBag.Sethighscore = "New High score";
+                    }else
+                    {
+                        var high = Convert.ToInt32(highscore);
+
+                        if (high < count)
+                        {
+                            HttpContext.Response.Cookies.Append("higscore", Convert.ToString(count), options);
+                            ViewBag.Sethighscore = "New High score";
+                        }
+                    }
+                }
+                else
+                {
+                    ViewBag.Count = count;
+                    ViewBag.Message = guessing.Massage;
+                }
+            }
+            
+            return View();
+        }
+    }
+}
---
+++ LexiconMVC/Models/GuessingRepository.cs
@@ -0,0 +1,45 @@
+﻿
+namespace LexiconMVC.Models
+{
+    public class GuessingRepository : IGuessingRepository
+    {
+        public int Generat()
+        {
+            Random random = new Random();
+
+            int tal = random.Next(1, 101);
+            return tal;
+
+        }
+        public bool Guess(Guessing guessing, Int32 tal)
+        {
+            if (guessing.Number != tal)
+            {
+                if (tal > guessing.Number)
+                {
+                    guessing.Massage = "Guess to smal!";
+                    guessing.Guess = guessing.Guess + 1;
+                }
+
+                else if (tal < guessing.Number)
+                {
+                    guessing.Massage = "Guess to big!";
+                    guessing.Guess = guessing.Guess + 1;
+                }else
+                {
+                    guessing.Massage = "Invalid Number";
+                }
+
+                return false;
+            }
+            if (guessing.Number == tal)
+            {
+                guessing.Guess = guessing.Guess + 1;
+                return true;
+            }
+
+            guessing.Massage = "Invalid input";
+            return false;
+        }
+    }
+}
