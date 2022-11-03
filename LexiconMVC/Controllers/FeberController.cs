cb222efac2bf35154bef13ec8bf2a650d898113d
sky-wolf <sky-wolf@users.noreply.github.com>
2022-11-03 11:52:42 +0100

    Fever controller

---
+++ LexiconMVC/Controllers/FeberController.cs
@@ -0,0 +1,24 @@
+﻿using LexiconMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+using System.Reflection.Emit;
+
+namespace LexiconMVC.Controllers
+{
+    public class FeberController : Controller
+    {
+
+        public IActionResult Check()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(int input)
+        {
+
+            ViewBag.Temp = Feber.FeberCheck(input);
+            return View();
+        }
+    }
+}
--- LexiconMVC/Program.cs
+++ LexiconMVC/Program.cs
@@ -1,8 +1,19 @@
+using LexiconMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMvc();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(15);
+});
+builder.Services.AddScoped<IGuessingRepository, GuessingRepository>();
 
 var app = builder.Build();
 
+app.UseSession();
 app.UseStaticFiles();
 app.UseRouting();
 
@@ -10,6 +21,20 @@ if(app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
-app.MapDefaultControllerRoute();
+app.MapControllerRoute(
+    name: "GuessingGame",
+    pattern: "GuessingGame",
+    defaults: new { controller = "Guessing", action = "Guess" });
+
+app.MapControllerRoute(
+    name: "FeverCheck",
+    pattern: "FeverCheck",
+    defaults: new {controller = "Feber", action = "Check" });
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
