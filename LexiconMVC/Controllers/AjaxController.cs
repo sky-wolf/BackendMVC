1f9ee773ff15b2d719b385ed7d2e02223c0cbaa8
sky-wolf <sky-wolf@users.noreply.github.com>
2022-11-15 13:29:35 +0100

    Controller explod

---
+++ LexiconMVC/Controllers/AjaxController.cs
@@ -0,0 +1,94 @@
+﻿using LexiconMVC.Models;
+using LexiconMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LexiconMVC.Controllers
+{
+    public class AjaxController : Controller
+    {
+        public AjaxController()
+        {
+            if (PeopleViewModel.listOfPeople.Count == 0)
+            {
+                PeopleViewModel.SkapaLista();
+            }
+        }
+
+        public IActionResult Index(string searchString)
+        {
+            return View();
+            
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(CreatePresonViewModel createPresonViewModel)
+        {
+
+            if (ModelState.IsValid)
+            {
+                PeopleViewModel.listOfPeople.Add(new Person { Id = Guid.NewGuid().ToString(), Name = createPresonViewModel.Name, City = createPresonViewModel.City, PhoneNumber = createPresonViewModel.PhoneNumber });
+            }
+
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string id)
+        {
+            var resultat = PeopleViewModel.listOfPeople.FirstOrDefault(p => p.Id == id);
+            if (resultat != null)
+            {
+                PeopleViewModel.listOfPeople.Remove(resultat);
+            }
+
+            return StatusCode(StatusCodes.Status204NoContent);
+           
+        }
+
+        public IActionResult GetPeople()
+        {
+
+            PeopleViewModel peopleViewModel = new PeopleViewModel();
+            peopleViewModel.tempListe = PeopleViewModel.listOfPeople;
+            
+
+            return PartialView("_peoplePartial", peopleViewModel.tempListe);
+        }
+
+        public IActionResult GetDetails(string id)
+        {
+            Person person = PeopleViewModel.listOfPeople.FirstOrDefault(p => p.Id == id);
+            return PartialView("_personPartial", person);
+        }
+
+        [HttpPost]
+        public IActionResult Search(string searchString)
+        {
+
+            PeopleViewModel peopleViewModel = new PeopleViewModel();
+
+
+            var list = PeopleViewModel.listOfPeople;
+
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                var resultat = list.Where(p => p.Name.Contains(searchString)).ToList();
+                if (resultat.Count == 0)
+                {
+                    resultat = list.Where(p => p.City.Contains(searchString)).ToList();
+                }
+
+                peopleViewModel.tempListe = resultat;
+
+            }
+            else
+            {
+                peopleViewModel.tempListe = PeopleViewModel.listOfPeople;
+            }
+            //return View(peopleViewModel);
+            return PartialView("_peoplePartial", peopleViewModel.tempListe);
+        }
+    }
+}
--- LexiconMVC/Controllers/PeopleController.cs
+++ LexiconMVC/Controllers/PeopleController.cs
@@ -9,41 +9,39 @@ namespace LexiconMVC.Controllers
 {
     public class PeopleController : Controller
     {
-        private readonly ApplicationDbContext _applicationDbContext;
-        public PeopleController(ApplicationDbContext applicationDbContext)
+        
+        public PeopleController()
         {
-            //if (PeopleViewModel.listOfPeople.Count == 0)
-            //{
-            //    PeopleViewModel.SkapaLista();
-            //}
-            _applicationDbContext = applicationDbContext;
+            if (PeopleViewModel.listOfPeople.Count == 0)
+            {
+                PeopleViewModel.SkapaLista();
+            }
         }
 
         public IActionResult Index(string searchString)
         {
-            return View();
-            //PeopleViewModel peopleViewModel = new PeopleViewModel();
-            //peopleViewModel.tempListe = PeopleViewModel.listOfPeople;
+            PeopleViewModel peopleViewModel = new PeopleViewModel();
+            peopleViewModel.tempListe = PeopleViewModel.listOfPeople;
 
-            //var list = PeopleViewModel.listOfPeople;
+            var list = PeopleViewModel.listOfPeople;
 
 
-            //if (!String.IsNullOrEmpty(searchString))
-            //{
-            //    var resultat = list.Where(p => p.Name.Contains(searchString)).ToList();
-            //    if (resultat.Count == 0)
-            //    {
-            //        resultat = list.Where(p => p.City.Contains(searchString)).ToList();
-            //    }
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                var resultat = list.Where(p => p.Name.Contains(searchString)).ToList();
+                if (resultat.Count == 0)
+                {
+                    resultat = list.Where(p => p.City.Contains(searchString)).ToList();
+                }
 
-            //    peopleViewModel.tempListe = resultat;
+                peopleViewModel.tempListe = resultat;
 
-            //}
-            //else
-            //{
-            //    peopleViewModel.tempListe = PeopleViewModel.listOfPeople;
-            //}
-            //return View(peopleViewModel);
+            }
+            else
+            {
+                peopleViewModel.tempListe = PeopleViewModel.listOfPeople;
+            }
+            return View(peopleViewModel);
         }
 
 
@@ -53,82 +51,23 @@ namespace LexiconMVC.Controllers
 
             if (ModelState.IsValid)
             {
-                //PeopleViewModel.listOfPeople.Add(new Person { Id = Guid.NewGuid().ToString(), Name = createPresonViewModel.Name, City = createPresonViewModel.City, PhoneNumber = createPresonViewModel.PhoneNumber });
-                _applicationDbContext.Add(new Person { Id = Guid.NewGuid().ToString(), Name = createPresonViewModel.Name, City = createPresonViewModel.City, PhoneNumber = createPresonViewModel.PhoneNumber });
-                _applicationDbContext.SaveChanges();
+                PeopleViewModel.listOfPeople.Add(new Person { Id = Guid.NewGuid().ToString(), Name = createPresonViewModel.Name, City = createPresonViewModel.City, PhoneNumber = createPresonViewModel.PhoneNumber });
             }
 
-            return StatusCode(StatusCodes.Status201Created);
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
         public ActionResult Delete(string id)
         {
-            Person person = _applicationDbContext.Persons.FirstOrDefault(p => p.Id == id);
-            if (person != null)
+            var result = PeopleViewModel.listOfPeople.FirstOrDefault(p => p.Id == id);
+            if (result != null)
             {
-                //PeopleViewModel.listOfPeople.Remove(person);
-                _applicationDbContext.Persons.Remove(person);
-                _applicationDbContext.SaveChanges();
-            }
-            person = _applicationDbContext.Persons.FirstOrDefault(p => p.Id == id);
-            if (person == null)
-            {
-                return StatusCode(StatusCodes.Status204NoContent);
-            }else
-            {
-                return StatusCode(StatusCodes.Status422UnprocessableEntity);
+                PeopleViewModel.listOfPeople.Remove(result);
             }
             
+            return RedirectToAction("Index");
         }
 
-        public IActionResult GetPeople()
-        {
-           
-            //PeopleViewModel peopleViewModel = new PeopleViewModel();
-            //peopleViewModel.tempListe = PeopleViewModel.listOfPeople;
-            List<Person> person = _applicationDbContext.Persons.ToList();
-            //_applicationDbContext
-            
-            return PartialView("_peoplePartial", person);
-        }
-
-        public IActionResult GetDetails(string id)
-        {
-            Person person = _applicationDbContext.Persons.FirstOrDefault(p => p.Id == id); /*PeopleViewModel.listOfPeople.FirstOrDefault(p => p.Id == id);*/
-            return PartialView("_personPartial",person);
-        }
-
-        [HttpPost]
-        public IActionResult Search(string searchString)
-        {
-
-            //PeopleViewModel peopleViewModel = new PeopleViewModel();
-
-
-            //var list = PeopleViewModel.listOfPeople;
-
-            List<Person> person;
-            if (!String.IsNullOrEmpty(searchString))
-            {
-                person = (from p in _applicationDbContext.Persons.Where(p => p.Name.Contains(searchString)) select p).ToList();
-
-                //person = _applicationDbContext.Persons.Where(p => p.Name.Contains(searchString)).ToList();
-                if (person.Count == 0)
-                {
-                    //person = _applicationDbContext.Persons.Where(p => p.City.Contains(searchString)).ToList();
-                    person = (from p in _applicationDbContext.Persons.Where(p => p.City.Contains(searchString)) select p).ToList();
-                }
-
-                //peopleViewModel.tempListe = resultat;
-
-            }
-            else
-            {
-                person = _applicationDbContext.Persons.ToList();
-            }
-            //return View(peopleViewModel);
-            return PartialView("_peoplePartial", person);
-        }
     }
 }
